8c0fee639b57356fe26e592a3316a2b9c2c2d535
InstaTrapCorp <58869886+InstaTrapCorp@users.noreply.github.com>
2022-08-13 00:35:57 +0800

    Update bot.py
    Bot is alive. Testing bot in progress. Token .env stored locally on server.

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@
 
 import os
 import discord
+import time
+
 from dotenv import load_dotenv
 
 class MyClient(discord.Client):
@@ -11,10 +13,11 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello World!')
+        if message.content.startswith('iwubchu'):
+            await message.channel.send("iwubchutoo")
 
 load_dotenv()
 token = os.getenv("BOT_TOKEN")
 client = MyClient()
 client.run(token)
+time.sleep(5)
